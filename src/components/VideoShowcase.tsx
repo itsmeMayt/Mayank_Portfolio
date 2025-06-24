0e77961520a9731b397fafb7de289478d15520f9
itsmeMayt <psychomayank0@gmail.com>
2025-06-24 16:43:30 +0530

    Update VideoShowcase component and other changes

--- src/components/VideoShowcase.tsx
+++ src/components/VideoShowcase.tsx
@@ -23,7 +23,7 @@ const videos: Video[] = [
     id: '1',
     title: 'Motion Ad for brand',
     description: 'A dynamic motion graphics advertisement showcasing cutting-edge technology with sleek animations and modern design elements.',
-    thumbnail: 'https://images.unsplash.com/photo-1611162617213-7d7a39e9b1d7?w=800&auto=format&fit=crop&q=60',
+    thumbnail: 'https://images.unsplash.com/photo-1729179664878-49b8cc390d2b?w=800&auto=format&fit=crop&q=60',
     videoUrl: 'https://cht36xxrzycrr1fo.public.blob.vercel-storage.com/Videos/iris_ad_final-yxm7pO07xSG1bHy78nLds7euMNBFFN.mp4',
     category: 'Motion Graphics'
   },
@@ -31,7 +31,7 @@ const videos: Video[] = [
     id: '2',
     title: 'Brand Promotion Video',
     description: 'A dynamic motion graphics advertisement showcasing cutting-edge technology with sleek animations and modern design elements.',
-    thumbnail: 'https://images.unsplash.com/photo-1550751827-4bd374c3f58b?w=800&auto=format&fit=crop&q=60',
+    thumbnail: 'https://images.unsplash.com/photo-1738533614668-0a1a2501a138?w=800&auto=format&fit=crop&q=60',
     videoUrl: '/videos/background.mp4',
     category: 'Motion Graphics'
   },
@@ -45,6 +45,47 @@ const videos: Video[] = [
   },
   {
     id: '4',
+    title: 'Motion Graphics Reel',
+    description: 'A dynamic motion graphics reel with sleek animations and modern design elements.',
+    thumbnail: 'https://images.unsplash.com/photo-1605106702842-01a887a31122?w=800&auto=format&fit=crop&q=60',
+    videoUrl: 'https://cht36xxrzycrr1fo.public.blob.vercel-storage.com/Videos/algorithm_final-LXrRtXWsscMWHKaRUp8GlgwphLEDgE.mp4',
+    category: 'Motion Graphics'
+  },
+  {
+    id: '5',
+    title: 'YT Longform',
+    description: 'A dynamic motion graphics reel with sleek animations and modern design elements.',
+    thumbnail: 'https://images.unsplash.com/photo-1746608942838-a484d55ffeda?w=800&auto=format&fit=crop&q=60',
+    videoUrl: 'https://drive.google.com/file/d/1t_aw5-imhxmUR5ZdkHLJMowsxdQ7RgYy/view?usp=drive_link',
+    category: 'YT Longform'
+  },
+  {
+    id: '6',
+    title: 'Documentary',
+    description: 'A documentary showcasing the journey of a person.',
+    thumbnail: 'https://images.unsplash.com/photo-1686416653754-a3c5522fd922?w=800&auto=format&fit=crop&q=60',
+    videoUrl: 'https://cht36xxrzycrr1fo.public.blob.vercel-storage.com/Videos/Documentary_final-qVQDf4bPMqQmBBAiIAnYMsonlRs6c4.mp4',
+    category: 'Documentary'
+  },
+  {
+    id: '7',
+    title: 'History Documentary',
+    description: 'A documentary showcasing the old history of a place.',
+    thumbnail: 'https://images.unsplash.com/photo-1582072291448-5db861a71699?w=800&auto=format&fit=crop&q=60',
+    videoUrl: 'https://drive.google.com/file/d/1kUichUM6LejbzrWJXU34OvUKuigXOV1U/view?usp=drive_link',
+    category: 'Documentary'
+  },
+
+  {
+    id: '8',
+    title: 'Typography',
+    description: 'A typography video showcasing the journey of a person.',
+    thumbnail: 'https://images.unsplash.com/photo-1662947201279-a2e44d01831f?w=800&auto=format&fit=crop&q=60',
+    videoUrl: 'https://cht36xxrzycrr1fo.public.blob.vercel-storage.com/Videos/sg02-EBmLXTHkNlymB28U0HxEljnIrALIM9.mp4',
+    category: 'Typography'
+  },
+  {
+    id: '9',
     title: 'Action Edit',
     description: 'A thrilling action edit showcasing dynamic camera movements.',
     thumbnail: 'https://images.unsplash.com/photo-1552820728-8b83bb6b773f?w=800&auto=format&fit=crop&q=60',
@@ -52,7 +93,7 @@ const videos: Video[] = [
     category: 'Action'
   },
   {
-    id: '5',
+    id: '10',
     title: 'Short Film - The Lost Self',
     description: 'A compelling narrative short film exploring themes of self-discovery and growth.',
     thumbnail: 'https://images.unsplash.com/photo-1536440136628-849c177e76a1?w=800&auto=format&fit=crop&q=60',
@@ -60,19 +101,20 @@ const videos: Video[] = [
     category: 'Short Films'
   },
   {
-    id: '6',
+    id: '11',
     title: 'College Fest Video',
     description: 'A vibrant and energetic video showcasing the excitement and energy of a college fest.',
     thumbnail: 'https://images.unsplash.com/photo-1516450360452-9312f5e86fc7?w=800&auto=format&fit=crop&q=60',
     videoUrl: 'https://cht36xxrzycrr1fo.public.blob.vercel-storage.com/Videos/inci_aip_band-FE0bT4wBMlWlmta6qyAMBUna8MgLHx.mp4',
     category: 'Events'
-  }
+  },
+  
 ]
 
 export default function VideoShowcase() {
   const [activeCategory, setActiveCategory] = useState('All')
   const [selectedVideo, setSelectedVideo] = useState<Video | null>(null)
-  const categories = ['All','Motion Graphics','Action','Typography','Events','Short Films','Others']
+  const categories = ['All','Motion Graphics','YT Longform','Documentary','Action','Typography','Events','Short Films','Others']
 
   const filteredVideos = activeCategory === 'All'
     ? videos
